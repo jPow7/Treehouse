b200da179d4c31f774eea723a43e8ddbe0635a75
jpow7 <powell.jordan07@gmail.com>
2017-05-28 01:46:37 -0400

    Removed Example code

--- Messy/src/Messy.java
+++ Messy/src/Messy.java
@@ -3,20 +3,23 @@ import java.util.List;
 
 public class Messy {
     public static void main(String[] args) {
+/*
         System.out.println("one");
         System.out.println("two");
         System.out.println("three");
         System.out.println("four");
         System.out.println("five");
+*/
 /*        this line as well with a hotkey that does multi-line commenting
         Please comment out this line and*/
-        List<String> numberWords = Arrays.asList("six", "seven", "eight", "nine");
-        for (String numberWord : numberWords) {
+
+//        List<String> numberWords = Arrays.asList("six", "seven", "eight", "nine");
+/*        for (String numberWord : numberWords) {
 //          Use the sout shortcut to write out numberWord;
             System.out.println(numberWord);
 
-        }
-        List<String> words = Arrays.asList("Bacon", "Eggs", "Cheese", "sandwich" );
-        words.forEach(System.out::println);
+        }*/
+        List<String> words = Arrays.asList("Bacon", "Eggs", "Cheese", "hot sauce");
+        words.forEach(s -> System.out.printf("I want %s %n", s));
     }
 }
